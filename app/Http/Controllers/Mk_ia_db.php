c177dd40e824f5827834baa697e63ea6fc83f9c7
Mario Gzman <makroz@hotmail.com>
2020-04-13 02:44:32 -0400

    arreglo autenticacion y can del front

--- app/Http/Controllers/Mk_ia_db.php
+++ app/Http/Controllers/Mk_ia_db.php
@@ -24,6 +24,7 @@ trait Mk_ia_db
     public function proteger($act='',$controler=''){
 
         if (isset($this->_autorizar)) {
+            Mk_debug::msgApi(['protger autorizar',$this->_autorizar]);
             if (empty($controler)){
                 if (!empty($this->_autorizar)){
                     $controler=$this->_autorizar;
